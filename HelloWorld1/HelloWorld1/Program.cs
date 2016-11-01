9597085bc523ba1ff5f63ff90a2d7a3ffc040d06
Alexey-Petukhov <hertaras.hertaras@gmail.com>
2016-11-01 21:28:36 +0500

    try to refactoring

--- HelloWorld1/HelloWorld1/Program.cs
+++ HelloWorld1/HelloWorld1/Program.cs
@@ -21,92 +21,70 @@ namespace vkSmartWall
             
             Console.WriteLine("----------------------------------------------------------------");
 
-            String uid1 = "42560016";
-            String uid2 = "71985644";
-            String uid3 = "53516640";
-            String uid4 = vkClient.GetUserById("mr.pavlichenkov").GetUid().ToString();
-
-            var wall = new AppWall();
-            wall = vkClient.GetWall(uid2);
-            //List<AppWallItem> wallItems = vkAPI.GetWall(uid2, 10);
-            //String str = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(1475332312).ToString();
-            //wallItems = vkAPI.GetWall(uid2, 46);
-            //wallItems = vkAPI.GetWall(uid2, 100);
-            //wallItems = vkAPI.GetWall(uid2, 101);
-            //wallItems = vkAPI.GetWall(uid2, 105);
-            //wallItems = vkAPI.GetWall(uid2, 120);
-
-            AppUser testAppUser = vkClient.GetUserById(uid2);
-            testAppUser.SetFriends(vkClient.GetFriends(testAppUser.GetUid().ToString()));
-            
-            //List<AppWallItem> userNews = vkAPI.GetNews(testAppUser);
-            ////userNews = userNews.OrderByDescending(o => o.GetLikes().count).ToList();
-            //// сначала сортировка по приоритетам, затем по максимальному количеству лайков.
-            //userNews = userNews.OrderBy(o => o.GetLikesPriority()).ThenByDescending(o => o.GetLikes().count).ToList();
-
-
-            
-            AppGroup appGroup = new AppGroup(gId);
+            //String uid1 = "42560016"; String uid2 = "71985644"; String uid3 = "53516640"; String uid4 = vkClient.GetUserById("mr.pavlichenkov").Uid.ToString();
+           
+            var appGroup = new AppGroup(gId);
             appGroup.Users = vkClient.GetMembers(appGroup.GroupName); // gets AppGroup members with their friends
             
             //// запись в файл дерева -----------------------------------------------------------------------------
-            int uCounter = 0;
-            int fCounter = 0;
+            int userCounter = 0;
+            int friendsCounter;
+            //File.Create(@"..\..\members+friends.txt");
+            File.Delete(@"..\..\members+friends.txt");
             foreach (var memb in appGroup.Users)
             {
-                uCounter++;
+                userCounter++;
                 File.AppendAllText(@"..\..\members+friends.txt",
-                    "~(№ " + uCounter + ") id = " + memb.GetUid() +
-                    " - " + memb.GetFirstname() + " " + memb.GetLastname() +
-                    ". Friends count:" + memb.GetFriends().Count + "\r\n");
-                fCounter = 0;
-                foreach (var friend in memb.GetFriends())
+                    "~(№ " + userCounter + ") id = " + memb.Uid +
+                    " - " + memb.FirstName + " " + memb.LastName +
+                    ". Friends count:" + memb.Friends.Count + "\r\n");
+                friendsCounter = 0;
+                foreach (var friend in memb.Friends)
                 {
-                    fCounter++;
+                    friendsCounter++;
                     File.AppendAllText(@"..\..\members+friends.txt",
-                        "-----(№ " + fCounter + ") id = " + friend.GetUid() + " - " +
-                        friend.GetFirstname() + " " +
-                        friend.GetLastname() + "\r\n");
+                        "-----(№ " + friendsCounter + ") id = " + friend.Uid + " - " +
+                        friend.FirstName + " " +
+                        friend.LastName + "\r\n");
                 }
             }
             
             ////------------------------------------------------------------------------------------------------------------
-
+            // вывод списка пользователей группы в консоль
             int cnt = 0;
             foreach (var u in appGroup.Users)
             {
                 cnt++;
-                Console.WriteLine("(№ " + cnt + ") id = " + u.GetUid() + " --- " + u.GetFirstname() + " " + u.GetLastname());
+                Console.WriteLine("(№ " + cnt + ") id = " + u.Uid + " --- " + u.FirstName + " " + u.LastName);
             }
+            // конец вывода списка пользователей группы
             Console.WriteLine("Теперь введите id пользователя, чтобы получить список его друзей:");
             String neededId = Console.ReadLine();
-            //AppUser us = new AppUser();
-            AppUser usr = appGroup.Users.Find(x => x.GetUid() == int.Parse(neededId));//vkAPI.GetUserById(neededId);
+            AppUser appUser = appGroup.Users.Find(x => x.Uid == int.Parse(neededId));
             /// !!! список друзей
             
-            List<AppUser> friends = usr.GetFriends();//vkAPI.GetFriends(neededId);
-            Console.WriteLine("Друзья пользователя " + usr.GetFirstname() + " " + usr.GetLastname() + "");
+            List<AppUser> friends = appUser.Friends;
+            Console.WriteLine("Друзья пользователя " + appUser.FirstName + " " + appUser.LastName + "");
             foreach (var friend in friends)
             {
-                Console.WriteLine("---" + friend.GetFirstname() + " " + friend.GetLastname());
+                Console.WriteLine("---" + friend.FirstName + " " + friend.LastName);
             }
             /// !!! - список новостей
 
-            AppWall userNews = vkClient.GetNews(usr);
-            //userNews = userNews.OrderByDescending(o => o.GetLikes().count).ToList();
+            AppWall userNews = vkClient.GetNews(appUser);
             // сначала сортировка по приоритетам, затем по максимальному количеству лайков.
             userNews.Items = userNews.Items.OrderBy(o => o.LikesPriority).ThenByDescending(o => o.GetLikes().count).ToList();
 
-            Console.WriteLine("Новости пользователя " + testAppUser.GetFirstname() + " " + testAppUser.GetLastname());
-            int newsCnt = 0;
-            int chCnt = 0;
-            int atCnt = 0;
-            //File.Create(@"..\..\newsForTestUser.txt");
+            Console.WriteLine("Новости пользователя " + appUser.FirstName + " " + appUser.LastName);
+            int newsCount = 0;
+            int copyHistoryCount = 0;
+            int attachmentsCount = 0;
+            File.Delete(@"..\..\newsFor-"+ appUser.Uid+ "-User.txt");
             foreach (var newsItem in userNews.Items)
             {
-                newsCnt++;
+                newsCount++;
 
-                File.AppendAllText(@"..\..\newsForTestUser.txt", "№ " + newsCnt + " +-- newsText: " + newsItem.Text + "\r\n" +
+                File.AppendAllText(@"..\..\newsFor-" + appUser.Uid + "-User.txt", "№ " + newsCount + " +-- newsText: " + newsItem.Text + "\r\n" +
                     "     --- id владельца: " + newsItem.OwnerId + "\r\n" +
                     "     --- date: " + new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(newsItem.Date).ToString() + "\r\n" +
                     "     --- likes: " + newsItem.GetLikes().count + "\r\n" +
@@ -114,38 +92,25 @@ namespace vkSmartWall
                     "     --- comments: " + newsItem.GetComments().count + "\r\n"
                     );
 
-
-                //Console.WriteLine("№ " + newsCnt + " +-- newsText: " + newsItem.GetText());
-                //Console.WriteLine("     --- id владельца: " + newsItem.GetOwnerId());
-                //Console.WriteLine("     --- date: " + new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(newsItem.GetDate()).ToString());
-                //Console.WriteLine("     --- likes: " + newsItem.GetLikes().count);
-                //Console.WriteLine("     --- reposts: " + newsItem.GetReposts().count);
-                //Console.WriteLine("     --- comments: " + newsItem.GetComments().count);
                 if (newsItem.GetAttachment2() != null)
                 {
-                    File.AppendAllText(@"..\..\newsForTestUser.txt", "     --- newsAttachments: " + "\r\n");
+                    File.AppendAllText(@"..\..\newsFor-" + appUser.Uid + "-User.txt", "     --- newsAttachments: " + "\r\n");
 
-                    //Console.WriteLine("     --- newsAttachments: ");
-                    atCnt = 0;
+                    attachmentsCount = 0;
                     foreach (var attachment2 in newsItem.GetAttachment2())
                     {
-                        atCnt++;
-                        File.AppendAllText(@"..\..\newsForTestUser.txt", "     --- --- Вложение № " + atCnt + "\r\n" +
+                        attachmentsCount++;
+                        File.AppendAllText(@"..\..\newsFor-" + appUser.Uid + "-User.txt", "     --- --- Вложение № " + attachmentsCount + "\r\n" +
                             "     --- ~~~ тип вложения " + attachment2.type + "\r\n"
                             );
 
-
-                        //Console.WriteLine("     --- --- Вложение № " + chCnt);
-                        //Console.WriteLine("     --- ~~~ тип вложения " + attachment2.type);
                         if (attachment2.type.Equals("photo"))
                         {
-                            File.AppendAllText(@"..\..\newsForTestUser.txt", "     --- ~~~ фото: " + attachment2.photo.photo_130 + "\r\n");
-                            //Console.WriteLine("     --- ~~~ фото: " + attachment2.photo.photo_130);
+                            File.AppendAllText(@"..\..\newsFor-" + appUser.Uid + "-User.txt", "     --- ~~~ фото: " + attachment2.photo.photo_130 + "\r\n");
                         }
                         else if (attachment2.type.Equals("audio"))
                         {
-                            File.AppendAllText(@"..\..\newsForTestUser.txt", "     --- ~~~ аудио: " + attachment2.audio.url + "\r\n");
-                            //Console.WriteLine("     --- ~~~ аудио: " + attachment2.audio.url);
+                            File.AppendAllText(@"..\..\newsFor-" + appUser.Uid + "-User.txt", "     --- ~~~ аудио: " + attachment2.audio.url + "\r\n");
                         }
 
                     }
@@ -153,42 +118,20 @@ namespace vkSmartWall
 
                 if (newsItem.GetCopyHistory() != null)
                 {
-                    File.AppendAllText(@"..\..\newsForTestUser.txt", "     --- newsCopyHistory: " + "\r\n");
-                    //Console.WriteLine("     --- newsCopyHistory: ");
-                    chCnt = 0;
+                    File.AppendAllText(@"..\..\newsFor-" + appUser.Uid + "-User.txt", "     --- newsCopyHistory: " + "\r\n");
+                    copyHistoryCount = 0;
                     foreach (var copyHistoryItem in newsItem.GetCopyHistory())
                     {
-                        chCnt++;
-                        File.AppendAllText(@"..\..\newsForTestUser.txt", "     --- --- Вложение № " + chCnt + "\r\n" +
+                        copyHistoryCount++;
+                        File.AppendAllText(@"..\..\newsFor-" + appUser.Uid + "-User.txt", "     --- --- Вложение № " + copyHistoryCount + "\r\n" +
                             "     --- ~~~ CHText: " + copyHistoryItem.text + "\r\n" +
                             "     --- ~~~ CHOwnerId: " + copyHistoryItem.owner_id + "\r\n" +
                             "     --- ~~~ CHDate: " + new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(copyHistoryItem.date).ToString() + "\r\n"
                             );
-                        //Console.WriteLine("     --- --- Вложение № " + chCnt);
-                        //Console.WriteLine("     --- ~~~ CHText: " + copyHistoryItem.text);
-                        //Console.WriteLine("     --- ~~~ CHOwnerId: " + copyHistoryItem.owner_id);
-                        //Console.WriteLine("     --- ~~~ CHDate: " + new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(copyHistoryItem.date).ToString());
-
                     }
 
                 }
             }
-            //List<AppWallItem> userNews = vkAPI.
-
-                //File.WriteAllText(@"D:\Documents\Visual Studio 2013\Projects\HelloWorld1\usr1Friends.txt", vkAPI.GetFriends(uid1));
-            //FriendsIdsList friendIds = JsonConvert.DeserializeObject<FriendsIdsList>(vkAPI.GetFriends(uid1));
-            //List<AppUser> friends = new List<AppUser>();
-            //foreach (var u in friendIds.response)
-            //{
-                
-            //    friends.Add(vkAPI.GetUserById(u.ToString()));
-            //}
-            //AppUser usr = vkAPI.GetUserById(uid1);
-            //Console.WriteLine("Друзья пользователя " + usr.GetFirstname() + " " + usr.GetLastname() + "");
-            //foreach (var friend in friends)
-            //{
-            //   Console.WriteLine("---" + friend.GetFirstname() + " " + friend.GetLastname());
-            //}
             Console.WriteLine("----------------------------------------------------------------");
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
---
+++ HelloWorld1/HelloWorld1/VkClientConverter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vkSmartWall
+{
+    public class VkClientConverter
+    {
+        VkAPI vkApi;
+        public VkClientConverter() 
+        {
+            vkApi = new VkAPI();
+        }
+
+        public AppUser Convert(VkApiUser vkApiUser)
+        {
+            var appUser = new AppUser(vkApiUser.id);
+            appUser.FirstName = vkApiUser.first_name;
+            appUser.LastName = vkApiUser.last_name;
+            return appUser;
+        }
+        public AppUser Convert(Member member) // for group member
+        {
+            var appUser = new AppUser(member.id);
+            appUser.FirstName = member.first_name;
+            appUser.LastName = member.last_name;
+            //appUser.Friends = vkClient.GetFriends(appUser.Uid.ToString());
+            //appUser.Wall = vkClient.GetWall(appUser.Uid.ToString() );
+            return appUser;
+        }
+
+        public AppUser Convert(Friend friend) // for group friend
+        {
+            var appUser = new AppUser(friend.id);
+            appUser.FirstName = friend.first_name;
+            appUser.LastName = friend.last_name;
+            //appUser.Friends = vkClient.GetFriends(appUser.Uid.ToString());
+            //AppUser.Wall = vkClient.GetWall(appUser.Uid.ToString() );
+            return appUser;
+        }
+
+        public AppWallItem Convert(Item wallItem)
+        {
+            var appWallItem = new AppWallItem();
+            appWallItem.Id = wallItem.id;
+            appWallItem.FromId = wallItem.from_id;
+            appWallItem.OwnerId = wallItem.owner_id;
+            appWallItem.Date = wallItem.date;
+            appWallItem.Text = wallItem.text;
+            appWallItem.SetLikes(wallItem.likes);
+            appWallItem.SetReposts(wallItem.reposts);
+            appWallItem.SetComments(wallItem.comments);
+            appWallItem.SetCopyHistory(wallItem.copy_history);
+            appWallItem.SetAttachment2(wallItem.attachments);
+
+            return appWallItem;
+        }
+
+        public List<AppUser> Convert(VkApiFriends vkApiFriends)
+        {
+            List<AppUser> friends = new List<AppUser>();
+            foreach (var friend in vkApiFriends.response.items)
+            {
+                //friends.Add(CreateAppUser(friend));
+                friends.Add(Convert(friend));
+            }
+            return friends;
+        }
+
+        public AppWall Convert(VkApiWall vkApiWall)
+        {
+            AppWall appWall = new AppWall();
+            foreach (var item in vkApiWall.response.items)
+            {
+                appWall.Items.Add(Convert(item));
+            }
+            return appWall;
+        }
+    }
+
+}
