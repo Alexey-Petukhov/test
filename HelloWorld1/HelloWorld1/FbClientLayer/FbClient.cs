9dc67ad14f90776709631db032d3d2af99f331be
Alexey-Petukhov <hertaras.hertaras@gmail.com>
2016-11-18 21:43:01 +0500

    Создал класс для работы с facebook share на уровне приложения

---
+++ HelloWorld1/HelloWorld1/FbClientLayer/FbClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vkSmartWall.FacebookApiLayer;
+using vkSmartWall.FacebookApiLayer.Model;
+using vkSmartWall.FbClientLayer.Model;
+
+namespace vkSmartWall.FbClientLayer
+{
+    class FbClient
+    {
+        FbAPI fbApi;
+        FbClientConverter fbClientConverter;
+
+        public FbClient(FbAPI fbApi)
+        {
+            this.fbApi = fbApi;
+            fbClientConverter = new FbClientConverter();
+        }
+
+        public AppFbShareCount GetFbSharesFromLink(string link)
+        {
+            var fbApiAnswer = fbApi.GetFbSharesFromLink(link);
+            var appFbShareCount = fbClientConverter.Convert(fbApiAnswer);
+            return appFbShareCount;
+        }
+
+    }
+}
