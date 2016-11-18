0d0f636578227721def7da5c7f8634b619fc9373
Alexey-Petukhov <hertaras.hertaras@gmail.com>
2016-11-18 21:41:16 +0500

    Создал класс для работы с facebook share api

---
+++ HelloWorld1/HelloWorld1/FacebookApiLayer/FbAPI.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using vkSmartWall.FacebookApiLayer.Model;
+
+namespace vkSmartWall.FacebookApiLayer
+{
+    public class FbAPI
+    {
+        private String serviceUrl = "";
+
+        public FbAPI()
+        {
+            serviceUrl = "http://graph.facebook.com/?id=";
+        }
+
+        public FbApiAnswer GetFbSharesFromLink(string link)
+        {
+            var url = serviceUrl + link;
+            var jsonAnswer = DoReqGet(url);
+            FbApiAnswer fbApiAnswer = new FbApiAnswer();
+            if (jsonAnswer != null)
+            {
+                fbApiAnswer = JsonConvert.DeserializeObject<FbApiAnswer>(jsonAnswer);
+            }
+
+            return fbApiAnswer;
+
+        }
+
+        public String DoReqGet(String url) // do GET
+        {
+            string reply = null;
+            using (WebClient client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                try
+                {
+                    reply = client.DownloadString(url);
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            return reply;
+        }
+    }
+}
