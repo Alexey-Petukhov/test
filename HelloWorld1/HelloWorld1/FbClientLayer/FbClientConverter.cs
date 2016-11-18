41b4e4d0605d52201bd9a61d390ae0468d9e959d
Alexey-Petukhov <hertaras.hertaras@gmail.com>
2016-11-18 21:44:56 +0500

    Конвертер из facebook share api в facebook share уровня приложения

---
+++ HelloWorld1/HelloWorld1/FbClientLayer/FbClientConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vkSmartWall.FacebookApiLayer;
+using vkSmartWall.FacebookApiLayer.Model;
+using vkSmartWall.FbClientLayer.Model;
+
+namespace vkSmartWall.FbClientLayer
+{
+    public class FbClientConverter
+    {
+        FbAPI fbApi;
+
+        public FbClientConverter()
+        {
+            fbApi = new FbAPI();
+        }
+
+        public AppFbShareCount Convert(FbApiAnswer fbApiAnswer)
+        {
+            var appFbShareCount = new AppFbShareCount();
+            appFbShareCount.ShareCount = fbApiAnswer.share != null ? fbApiAnswer.share.share_count : 0;
+            return appFbShareCount;
+        }
+    }
+}
