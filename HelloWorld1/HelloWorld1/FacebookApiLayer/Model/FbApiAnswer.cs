e766de9002f82fd8acc2b84fb2bebda7ad4b85d6
Alexey-Petukhov <hertaras.hertaras@gmail.com>
2016-11-18 21:41:57 +0500

    Класс для создания объекта facebook share api

---
+++ HelloWorld1/HelloWorld1/FacebookApiLayer/Model/FbApiAnswer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vkSmartWall.FacebookApiLayer.Model
+{
+    
+    public class OgObject
+    {
+        public string id { get; set; }
+        public string description { get; set; }
+        public string title { get; set; }
+        public string type { get; set; }
+        public string updated_time { get; set; }
+    }
+
+    public class Share
+    {
+        public int comment_count { get; set; }
+        public int share_count { get; set; }
+    }
+
+    public class FbApiAnswer
+    {
+        public OgObject og_object { get; set; }
+        public Share share { get; set; }
+        public string id { get; set; }
+    }
+}
