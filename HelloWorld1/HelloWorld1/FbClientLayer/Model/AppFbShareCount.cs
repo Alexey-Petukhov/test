ac78250dfd307db556e1322f61b34bf14d5957d2
Alexey-Petukhov <hertaras.hertaras@gmail.com>
2016-11-18 21:44:05 +0500

    класс для объекта facebook share уровня приложения

---
+++ HelloWorld1/HelloWorld1/FbClientLayer/Model/AppFbShareCount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vkSmartWall.FbClientLayer.Model
+{
+    public class AppFbShareCount
+    {
+        public int ShareCount { get; set; }
+
+        public AppFbShareCount()
+        {
+            
+        }
+    }
+}
